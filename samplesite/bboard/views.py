3533dc055abfefb03beafdde4de6b22deaf50488
Emperrorr <pshevchenko@mail.ru>
2021-06-02 11:02:29 +0300

    start project

---
+++ samplesite/bboard/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from .models import Bb
+
+def index (request):
+#    template=loader.get_template('bboard/index.html')
+    bbs=Bb.objects.all()
+#    context={'bbs':bbs}
+    return render (request, 'bboard/index.html', {'bbs':bbs})
+
+
+
+
+
+
+#from django.http import HttpResponse
+#from django.template import loader
+#from .models import Bb
+
+#def index (request):
+#    template=loader.get_template('bboard/index.html')
+#    bbs=Bb.objects.order_by('-published')
+#    context={'bbs':bbs}
+#    return HttpResponse(template.render (context, request))
